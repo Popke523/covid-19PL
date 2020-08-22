a1ebec512583f19c1df25a67a3d02308a26621d9
Popke <popke@popke.pl>
2020-08-23 00:01:18 +0200

    Code cleanup

--- covid19PL.py
+++ covid19PL.py
@@ -1,10 +1,17 @@
-import json,urllib.request
-req = urllib.request.Request(url="https://api.nowywirus.pl/", headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
+import json
+import urllib.request
+
+req = urllib.request.Request(
+    url="https://api.nowywirus.pl/",
+    headers={'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'}
+)
 data = urllib.request.urlopen(req).read()
 output = json.loads(data)
+
 ilosckoronawirusow = output[0]["cases"]
 iloscnowa = output[0]["new_cases"]
 deaths = output[0]["deaths"]
 deathstoday = output[0]["new_deaths"]
+
 print(f"Ilość aktywnych koronawirusów wynosi {ilosckoronawirusow}, przybyło nowych {iloscnowa} koronawirusów w Polsce.")
 print(f"Nie żyje już {deaths} osób, dzisiaj zmarło {deathstoday}.")
